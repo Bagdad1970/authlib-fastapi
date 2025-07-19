d5ca10ed907df861c8e901fd9589882d4f2d56e7
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-07-20 02:02:06 +0300

    Modify default_headers to match thhe FastAPI.Response class

--- authlib/consts.py
+++ authlib/consts.py
@@ -4,8 +4,17 @@ author = "Hsiaoming Yang <me@lepture.com>"
 homepage = "https://authlib.org"
 default_user_agent = f"{name}/{version} (+{homepage})"
 
+"""
 default_json_headers = [
     ("Content-Type", "application/json"),
     ("Cache-Control", "no-store"),
     ("Pragma", "no-cache"),
-]
+]"""
+
+default_json_headers = {
+    "Content-Type": "application/json",
+    "Cache-Control": "no-store",
+    "Pragma" : "no-cache",
+}
+
+
